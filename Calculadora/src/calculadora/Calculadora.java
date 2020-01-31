844cf7061d0d577df33b82dbd822168dd2657033
Patricia <pvillanueva.polo@gmail.com>
2020-01-31 22:00:50 +0100

    "Primer cambio en mi repositorio"

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -47,5 +47,9 @@ public class Calculadora {
 		return resul;
 		
 	}
+	
+	public String tabla() {
+		return "1 * 1 = 1\n1 * 2 = 2\n1 * 3 = 3\n1 * 4 = 4\n1 * 5 = 5\n1 * 6 = 6\n1 * 7 = 7\n1 * 8 = 8\n1 * 9 = 9\n1 * 10 = 10\n";
+	}
 
 }
